e80305ce5c3c3bbae487dedddcec183f2312f32b
kje7713-dev <kje7713@gmail.com>
2025-12-03 14:24:39 -0500

    Update SavageByDesignApp.swift

--- SavageByDesignApp.swift
+++ SavageByDesignApp.swift
@@ -1,4 +1,9 @@
-// SavageByDesignApp.swift
+//
+//  SavageByDesignApp.swift
+//  Savage By Design
+//
+//  Phase 3: App entry -> HomeView
+//
 
 import SwiftUI
 
@@ -7,6 +12,8 @@ struct SavageByDesignApp: App {
     var body: some Scene {
         WindowGroup {
             HomeView()
+                // If we ever want to override the theme, we can inject here:
+                // .environment(\.sbdTheme, .default)
         }
     }
 }
