f4f2882b52d7eb9279adb06f8f8e5823e4c41b47
kje7713-dev <kje7713@gmail.com>
2025-12-03 09:45:52 -0500

    Initialize SavageByDesignApp with HomeView

---
+++ SavageByDesignApp.swift
@@ -0,0 +1,12 @@
+// SavageByDesignApp.swift
+
+import SwiftUI
+
+@main
+struct SavageByDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
